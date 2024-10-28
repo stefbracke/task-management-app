3da265be0d8f4cd6dce2ae255214b8663555902e
Stef <stef.br@pixel-nexus.com>
2024-10-28 15:54:38 +0100

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { defineProps } from "vue";
+defineProps({ bg: { type: String, default: "gray-100" } });
+</script>
+<template>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
