733fcb9e8617b8000dc0504b97c1dce8f9bab000
Stef <stef.br@pixel-nexus.com>
2024-10-29 13:30:07 +0100

    Clean start

--- src/components/Card.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { defineProps } from "vue";
-defineProps({ bg: { type: String, default: "gray-100" } });
-</script>
-<template>
-  <div :class="`${bg} p-6 rounded-lg shadow-md`">
-    <slot></slot>
-  </div>
-</template>
